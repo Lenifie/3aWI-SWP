d11eebade727b1016de2e297a8a9c7134c189576
Lenifie <leoniesophie.stecher@gmx.at>
2024-09-22 15:06:26 +0200

    Aufgabe2

--- SWP_FirstSteps/MyFirstConsoleApp/Program.cs
+++ SWP_FirstSteps/MyFirstConsoleApp/Program.cs
@@ -6,9 +6,32 @@ while (true)
 {
     input = Console.ReadLine();
 
-    if(input == "Beenden")
+    if(input == "beenden")
     {
         break;
     }
     Console.WriteLine(input);
+}
+
+
+Console.WriteLine("Aufgabe 2");
+string input2 = string.Empty;
+int number = 0;
+
+while (true)
+{
+    input2 = Console.ReadLine();
+
+    if (input2 == "beenden")
+    {
+        break;
+    }
+
+    if(int.TryParse(input2 , out number)&& number >= 0){
+        Console.WriteLine("Number is: " + number);
+    }
+    else
+    {
+        Console.WriteLine("Not a number");
+    }
 }
